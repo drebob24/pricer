6e9c1ec4593a5dcc2eaf3a1b79b27e90c1bd3867
Andrius Bobbit <abobbit@gmail.com>
2025-01-10 15:43:25 +0000

    Create functionality to detect items on sale. Clean up code.

--- pricer.py
+++ pricer.py
@@ -1,6 +1,7 @@
 from arg_parser import get_args
 from barbora import get_barbora
 from rimi import get_rimi
+from process_results import process_results
 
 
 def main():
@@ -33,46 +34,11 @@ def main():
         rimi_list = get_rimi(args.search)
         print("Rimi search completed\n")
         if barbora_list or rimi_list:
-            cheapest_list, options_list = sort_lists(barbora_list, rimi_list, args.order)
-            cheapest_list, options_list = generate_item_text(cheapest_list), generate_item_text(options_list)
-            print(create_cheapest_output(cheapest_list))
-            print(create_options_output(options_list))
+            cheapest_items, options = process_results(barbora_list + rimi_list, args.order)
+            print(cheapest_items)
+            print(options)
         else:
             print(f"No Results for search: '{args.search}'")
-
-
-def sort_lists(list_a, list_b, order):
-    if order == "list":
-        sorted_list = sorted(list_a + list_b, key=lambda x: (x["list_price"],
-                                                            x["unit_price"],
-                                                            x["title"]))
-    if order == "unit":
-        sorted_list = sorted(list_a + list_b, key=lambda x: (x["unit_price"],
-                                                            x["title"]))
-    i = 0
-    cheapest_price = sorted_list[0][f"{order}_price"]
-    for item in sorted_list:
-        if item[f"{order}_price"] > cheapest_price:
-            break
-        i += 1
-    return sorted_list[0:i], sorted_list[i:]
-
-
-def generate_item_text(item_list):
-    return [f"{item["title"]}:\n{item["list_price"]} € ({item["unit_price"]} €/{item["unit"]}) at {item["store"]}" for item in item_list]
-
-
-def create_cheapest_output(items):
-    if len(items) == 1:
-        return f"The cheapest item is:\n{"\n".join(items)}\n"
-    else:
-        return f"The cheapest items are:\n{"\n".join(items)}\n"
-    
-
-def create_options_output(options):
-    options_text = "\n".join(options)
-    return f"Other options:\n{options_text}"
-
 
 
 
---
+++ process_results.py
@@ -0,0 +1,64 @@
+def process_results(results_list, sort_order):
+    merged_list = sort_lists(results_list, sort_order)
+    merged_list = check_for_sales(merged_list)
+    cheapest_list, options_list = split_cheapest(merged_list, sort_order)
+    cheapest_list, options_list = generate_item_text(cheapest_list), generate_item_text(options_list)
+    cheapest_text = create_cheapest_output(cheapest_list)
+    options_text = create_options_output(options_list)
+    return cheapest_text, options_text
+
+
+def sort_lists(item_list, order):
+    if order == "list":
+        sorted_list = sorted(item_list, key=lambda x: (x["list_price"],
+                                                            x["unit_price"],
+                                                            x["title"]))
+    if order == "unit":
+        sorted_list = sorted(item_list, key=lambda x: (x["unit_price"],
+                                                            x["title"]))
+    return sorted_list
+
+
+def split_cheapest(item_list, order):
+    i = 0
+    cheapest_price = item_list[0][f"{order}_price"]
+    for item in item_list:
+        if item[f"{order}_price"] > cheapest_price:
+            break
+        i += 1
+    return item_list[0:i], item_list[i:]
+
+
+def generate_item_text(item_list):
+    text_list = []
+    for item in item_list:
+        if item["on_sale"]:
+            item_text = f"{item["title"]}:\n{item["list_price"]} € ({item["unit_price"]} €/{item["unit"]}) at {item["store"]} -{get_discount(item)}% SALE"
+        else:
+            item_text = f"{item["title"]}:\n{item["list_price"]} € ({item["unit_price"]} €/{item["unit"]}) at {item["store"]}"
+        text_list.append(item_text)
+    return text_list
+
+
+def check_for_sales(item_list):
+    for item in item_list:
+        item["on_sale"] = item["list_price"] < item["retail_price"]
+        item["discount"] = get_discount(item)
+    return item_list
+    
+
+def get_discount(item):
+    discount_amount = (1 - item["list_price"]/item["retail_price"]) * 100
+    return int(round(discount_amount))
+
+
+def create_cheapest_output(items):
+    if len(items) == 1:
+        return f"The cheapest item is:\n{"\n".join(items)}\n"
+    else:
+        return f"The cheapest items are:\n{"\n".join(items)}\n"
+    
+
+def create_options_output(options):
+    options_text = "\n".join(options)
+    return f"Other options:\n{options_text}"
