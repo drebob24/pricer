aa3474028f1da207fea31cd812ee1744d813b205
Andrius Bobbit <abobbit@gmail.com>
2025-01-11 00:48:31 +0000

    Clean up main()

--- pricer.py
+++ pricer.py
@@ -13,49 +13,56 @@ def main():
     -Save watch list
         -Track if price goes up/down from last fetch
         -Save history of price data?
-    -Create list in program
+    -Create list in program (why?)
 
     -Make results more robust such as sorting by similarity to search term    
     '''
     args = get_args()
-
-    if args.items:
-        search_list = args.items
-    if args.file:
-        search_list = read_item_file(args.file)
-
+    search_list = get_search_list(args)
     if args.search:
-        max_items = 10
-        if len(search_list) > max_items:
-            raise ValueError(f"Error: At most {max_items} items may be searched per instance.")
-        print("Searching for items...")
-        search_results = []
-        for item in search_list:
-            barbora_list = get_barbora(item, args.results)
-            print(f"\nBarbora search '{item}' completed")
-            rimi_list = get_rimi(item, args.results)
-            print(f"Rimi search '{item}' completed\n")
-            if barbora_list or rimi_list:
-                sorted_results = process_results(barbora_list + rimi_list, item, args.order, args.save)
-                if args.save == "csv":
-                    search_results += sorted_results
-                    time.sleep(random.randint(2,5))
-                    continue
-                search_results.append(f"Results for '{item}':\n")
-                search_results.append(sorted_results)
-            else:
-                search_results.append(f"No Results for search: '{item}'")
-            if not args.save:
-                print("\n".join(search_results))
-            time.sleep(random.randint(2,5))
+        search_results = handle_item_search(args, search_list)
+        save_results(args, search_results)
 
 
+def get_search_list(args):
+    if args.items:
+        return args.items
+    if args.file:
+        return read_item_file(args.file)
+    
+
+def save_results(args, search_results):
     if args.save == "txt":
         write_results_txt(search_results)
-
-
     if args.save == "csv":
         write_results_csv(search_results)
+
+
+def handle_item_search(args, search_list):
+    max_items = 10
+    if len(search_list) > max_items:
+        raise ValueError(f"Error: At most {max_items} items may be searched per instance.")
+    print("Searching for items...")
+    search_results = []
+    for item in search_list:
+        barbora_list = get_barbora(item, args.results)
+        print(f"\nBarbora search '{item}' completed")
+        rimi_list = get_rimi(item, args.results)
+        print(f"Rimi search '{item}' completed\n")
+        if barbora_list or rimi_list:
+            sorted_results = process_results(barbora_list + rimi_list, item, args.order, args.save)
+            if args.save == "csv":
+                search_results += sorted_results
+                time.sleep(random.randint(2,5))
+                continue
+            search_results.append(f"Results for '{item}':\n")
+            search_results.append(sorted_results)
+        else:
+            if not args.save =="csv":
+                search_results.append(f"No Results for search: '{item}'")
+        if not args.save:
+            print("\n".join(search_results))
+        time.sleep(random.randint(2,5))
 
 
 if __name__ == "__main__":
